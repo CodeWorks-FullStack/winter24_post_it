e68a1d193f2babcf70dc76e3bf2af152e74cec91
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-09 12:24:30 -0700

    album schema

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -3,7 +3,6 @@ import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 
 class DbContext {
-  Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
 }
 
