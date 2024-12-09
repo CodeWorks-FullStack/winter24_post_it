a4de26617e3f118011726e435a562a42ba39a17f
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-09 14:02:20 -0700

    populating creator

--- server/src/services/AlbumsService.js
+++ server/src/services/AlbumsService.js
@@ -3,6 +3,7 @@ import { dbContext } from "../db/DbContext.js"
 class AlbumsService {
   async createAlbum(albumData) {
     const album = await dbContext.Albums.create(albumData)
+    await album.populate('creator', 'name picture')
     return album
   }
 }
