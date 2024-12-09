5614381806871847d9ebb134cbbac59784f54aab
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-09 15:03:41 -0700

    put albums in appstate

--- client/src/services/AlbumsService.js
+++ client/src/services/AlbumsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { AppState } from "../AppState.js"
+import { Album } from "@/models/Album.js"
 
 class AlbumsService {
   async getAlbums() {
     const response = await api.get('api/albums')
     logger.log('GOT ALBUMS 📔', response.data)
+    const albums = response.data.map(albumPOJO => new Album(albumPOJO))
+    AppState.albums = albums
   }
 }
 
