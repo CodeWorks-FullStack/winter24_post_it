005e97f301da164cbcfa53e6a3a97adf2e581e35
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-11 09:12:26 -0700

    registered watcher in dbcontext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { AlbumSchema } from '../models/Album.js';
+import { WatcherSchema } from '../models/Watcher.js';
 
 class DbContext {
   Account = mongoose.model('Account', AccountSchema);
   Albums = mongoose.model('Album', AlbumSchema);
+  Watchers = mongoose.model('Watcher', WatcherSchema);
 }
 
 export const dbContext = new DbContext()
