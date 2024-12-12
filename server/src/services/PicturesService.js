693bed6ea3002a96f6fd5a91795ffbd054d924cb
Jeremy Fowler <jembyfowler@gmail.com>
2024-12-12 09:29:28 -0700

    checking to see if album is archived before adding a picture

--- server/src/services/PicturesService.js
+++ server/src/services/PicturesService.js
@@ -1,7 +1,15 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
+import { albumsService } from "./AlbumsService.js"
 
 class PicturesService {
   async createPicture(pictureData) {
+    const album = await albumsService.getAlbumById(pictureData.albumId)
+
+    if (album.archived) {
+      throw new Forbidden(`${album.title} is archived and no longer accepting new pictures`)
+    }
+
     const picture = await dbContext.Pictures.create(pictureData)
     await picture.populate('creator', 'name picture')
     return picture
