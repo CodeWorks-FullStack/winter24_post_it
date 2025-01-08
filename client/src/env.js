faa2706bccdedcc38b4cfee3b4bed76e63f366d2
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-08 10:21:01 -0700

    enabled sockets

--- client/src/env.js
+++ client/src/env.js
@@ -1,7 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 // NOTE our baseURL should point to 3000 for our full stack applications
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
+export const useSockets = true
 export const domain = 'dev-h63x8ohlbl1q2qkp.us.auth0.com'
 export const clientId = 'XX15k7a9Be1KE1Usl1aaOrDdzKJwvtUp'
 export const audience = 'https://jeremyisaraddude.com'
